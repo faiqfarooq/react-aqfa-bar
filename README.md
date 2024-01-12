4a7b92c1be09693e00955cf7df23886b418c3ba6
faiqfarooq <faiq.farooq@magnusmage.com>
2024-01-12 15:11:10 +0500

    Readme updated

--- README.md
+++ README.md
@@ -1,3 +1,77 @@
 ﻿# react-aqfa-bar
 
-Introducing react-aqfa-bar, a powerful library that simplifies the creation of progress bars and various chart bars in React applications. With easy-to-use components, it provides a seamless way to visualize data and progress in a visually appealing manner.
+Beta Version
+<!-- [![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url] -->
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <!-- <a href="https://github.com/magnusmage/creatachain-explorer">
+    <img src="public/logo.png" alt="Logo" width="90" height="100">
+  </a> -->
+
+  <h3 align="center">React Aqfa Progress Chart Bar</h3>
+
+  <p align="center">
+  Introducing react-aqfa-bar, a powerful library that simplifies the creation of progress bars and various chart bars in React applications. With easy-to-use components, it provides a seamless way to visualize data and progress in a visually appealing manner.
+    <br />
+    <br />
+    <!-- <a href="https://creatachain.com">Download Demo</a> -->
+    <!-- ·
+    <a href="https://github.com/magnusmage/creatachain-admin/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/magnusmage/creatachain-admin/issues">Request Feature</a> -->
+  </p>
+</p>
+
+## Getting Started
+
+Install this library with your favorite package manager:
+
+```sh
+npm i react-aqfa-bar
+```
+
+or
+
+```sh
+yarn add react-aqfa-bar
+```
+
+Then, import and use it:
+
+```sh
+import { ProgressBar } from "react-aqfa-bar";
+
+ <ProgressBar
+          RaisedValue={50}
+          TotalValue={100}
+          ContainerStyle={{ width: "500px" }}
+        />
+```
+
+## Docs
+
+we are working on it
+
+- All done :smile:
+
+<!-- [contributors-shield]: https://img.shields.io/static/v1?label=CONTRIBUTORS&message=2&color=blue
+[contributors-url]: https://github.com/magnusmage/creatachain-admin/graphs/contributors
+[forks-shield]: https://img.shields.io/static/v1?label=FORKS&message=0&color=blue
+[forks-url]: https://github.com/magnusmage/creatachain-explorer/network/members
+[stars-shield]: https://img.shields.io/static/v1?label=STARS&message=0&color=blue
+[stars-url]: https://github.com/magnusmage/creatachain-admin/stargazers
+[issues-shield]: https://img.shields.io/static/v1?label=ISSUES&message=0&color=blue
+[issues-url]: https://github.com/magnusmage/creatachain-admin/issues
+[license-shield]: https://img.shields.io/static/v1?label=LICENSE&message=MIT&color=blue
+[license-url]: https://github.com/magnusmage/creatachain-admin/blob/master/LICENSE
+[product-screenshot]: https://i.ibb.co/5skxBw2/coverimage.png -->
+
+```
+Copyright © 2024 — aqfa.tech
+```
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 
-const ProgressBar = ({ RaisedValue, TotalValue, Cap, width }) => {
+const ProgressBar = ({
+  InitilValue,
+  TotalValue,
+  Cap,
+  initalText,
+  totalText,
+  InitiSymbol,
+  FinalSymbol,
+  ContainerStyle,
+}) => {
   const [values, setValues] = useState({ Total: 100, Raised: 10 });
   const SoftCap = (20 * values?.Total) / 100;
   const hardCap = (80 * values?.Total) / 100;
@@ -9,122 +18,129 @@ const ProgressBar = ({ RaisedValue, TotalValue, Cap, width }) => {
     if (values?.Total >= values?.Raised) {
       setValues({
         ...values,
-        Raised: Number(RaisedValue),
+        Raised: Number(InitilValue),
         Total: Number(TotalValue),
       });
     }
-  }, [RaisedValue, TotalValue]);
+  }, [InitilValue, TotalValue]);
 
   const progressBarWidth = `${(values?.Raised / values?.Total) * 100}%`;
-  const CustomWidth =
-    width === "xs"
-      ? "320px"
-      : width === "sm"
-      ? "600px"
-      : width === "md"
-      ? "800px"
-      : "100%";
 
   return (
-    <div style={{ minWidth: "300px", width: CustomWidth }}>
+    <div
+      style={{
+        minWidth: "280px",
+        width: "100%",
+        padding: "16px",
+        backgroundColor: "#F2F5F9",
+        borderRadius: "8px",
+        marginTop: "12px",
+        ...ContainerStyle,
+      }}
+    >
       <div
         style={{
-          padding: "16px",
-          backgroundColor: "#F2F5F9",
-          borderRadius: "8px",
-          marginTop: "12px",
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "12px",
+        }}
+      >
+        {initalText && (
+          <div>
+            <p style={{ color: "#1E1E1E" }}>{initalText}</p>
+            <h3 style={{ fontWeight: "500" }}>
+              {values?.Raised} {InitiSymbol}
+            </h3>
+          </div>
+        )}
+        {totalText && (
+          <div>
+            <p style={{ color: "#1E1E1E", textAlign: "right" }}>{totalText}</p>
+
+            <h3 style={{ fontWeight: "500" }}>
+              {values?.Total} {FinalSymbol}
+            </h3>
+          </div>
+        )}
+      </div>
+      <div
+        style={{
+          position: "relative",
+          minHeight: Cap === true ? "80px" : "auto",
         }}
       >
         <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginBottom: "12px",
+            width: "100%",
+            background: `linear-gradient(to right, #fff 0%, #fff 80%, rgba(69, 76, 84, 0.30) 80%, rgba(69, 76, 84, 0.30) 100%)`,
+            borderRadius: "32px",
+            zIndex: 100,
           }}
         >
-          <div>
-            <p style={{ color: "#1E1E1E" }}>Raised</p>
-            <h1 style={{ fontWeight: "500" }}>{values?.Raised} BRC</h1>
-          </div>
-          <div>
-            <p style={{ color: "#1E1E1E", textAlign: "right" }}>Total</p>
-            <h1 style={{ fontWeight: "500" }}>{values?.Total} BRC</h1>
-          </div>
-        </div>
-        <div style={{ position: "relative", minHeight: "80px" }}>
           <div
             style={{
-              width: "100%",
-              background: `linear-gradient(to right, #fff 0%, #fff 80%, rgba(69, 76, 84, 0.30) 80%, rgba(69, 76, 84, 0.30) 100%)`,
+              display: "flex",
+              alignItems: "center",
+              height: "7.9px",
+              marginBottom: "5px",
+              backgroundColor: "#013EB7",
+              width: progressBarWidth,
+              color: "white",
               borderRadius: "32px",
-              zIndex: 100,
+            }}
+          ></div>
+        </div>
+
+        {Cap === true && (
+          <div
+            style={{
+              position: "absolute",
+              top: "9.9%",
+              left: "20%",
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
             }}
           >
             <div
               style={{
-                display: "flex",
-                alignItems: "center",
-                height: "7.9px",
-                marginBottom: "5px",
-                backgroundColor: "#013EB7",
-                width: progressBarWidth,
-                color: "white",
-                borderRadius: "32px",
+                width: "2px",
+                height: "18px",
+                backgroundColor: "rgba(69, 76, 84, 0.30)",
+                marginBottom: "2px",
               }}
             ></div>
+            <div style={{ position: "absolute", top: "18px" }}>
+              <p style={{ color: "#1E1E1E" }}>Soft Cap</p>
+              <h1>{SoftCap}</h1>
+            </div>
           </div>
-
-          {Cap === true && (
+        )}
+        {Cap === true && (
+          <div
+            style={{
+              position: "absolute",
+              top: "9.9%",
+              right: "20%",
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
             <div
               style={{
-                position: "absolute",
-                top: "9.9%",
-                left: "20%",
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
+                width: "2px",
+                height: "18px",
+                backgroundColor: "rgba(69, 76, 84, 0.30)",
+                marginBottom: "2px",
               }}
-            >
-              <div
-                style={{
-                  width: "2px",
-                  height: "18px",
-                  backgroundColor: "rgba(69, 76, 84, 0.30)",
-                  marginBottom: "2px",
-                }}
-              ></div>
-              <div style={{ position: "absolute", top: "18px" }}>
-                <p style={{ color: "#1E1E1E" }}>Soft Cap</p>
-                <h1>{SoftCap}</h1>
-              </div>
+            ></div>
+            <div style={{ position: "absolute", top: "18px" }}>
+              <p style={{ color: "#1E1E1E" }}>Hard Cap</p>
+              <h1>{hardCap}</h1>
             </div>
-          )}
-          {Cap === true && (
-            <div
-              style={{
-                position: "absolute",
-                top: "9.9%",
-                right: "20%",
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <div
-                style={{
-                  width: "2px",
-                  height: "18px",
-                  backgroundColor: "rgba(69, 76, 84, 0.30)",
-                  marginBottom: "2px",
-                }}
-              ></div>
-              <div style={{ position: "absolute", top: "18px" }}>
-                <p style={{ color: "#1E1E1E" }}>Hard Cap</p>
-                <h1>{hardCap}</h1>
-              </div>
-            </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
